0e9203fae43f03970282bca990d2c37bd57c9d08
Saif Ur Rehman <fsdev.saifurrehman@gmail.com>
2025-07-23 19:01:50 +0500

    fixed the sitemap

--- app/Http/Service/SitemapService.php
+++ app/Http/Service/SitemapService.php
@@ -20,22 +20,22 @@ class SitemapService {
         $this->sitemap();
         $this->pages();
         $this->blogs();
-        $this->blogs_images();
-        $this->category_images();
+        // $this->blogs_images();
+        // $this->category_images();
         $this->category();
         $this->stores();
-        $this->stores_images();
+        // $this->stores_images();
     }
     public function sitemap()
     {
         $data  = [
             array('url' => url(asset($this->siteMapDirectory . '/pages.xml')), 'priority' => 0.8, 'changeFreq' => 'monthly', 'lastmod' => Carbon::now()->toIso8601String()),
             array('url' => url(asset($this->siteMapDirectory . '/blogs.xml')), 'priority' => 1.0, 'changeFreq' => 'daily', 'lastmod' => ($blog = Blog::latest('updated_at')->first()) ? ($blog->updated_at->gt($blog->created_at) ? $blog->updated_at->toIso8601String() : $blog->created_at->toIso8601String()) : Carbon::now()->toIso8601String()),
-            array('url' => url(asset($this->siteMapDirectory . '/blogs-images.xml')), 'priority' => 0.4, 'changeFreq' => 'monthly', 'lastmod' => ($blog = Blog::latest('updated_at')->first()) ? ($blog->updated_at->gt($blog->created_at) ? $blog->updated_at->toIso8601String() : $blog->created_at->toIso8601String()) : Carbon::now()->toIso8601String()),
+            // array('url' => url(asset($this->siteMapDirectory . '/blogs-images.xml')), 'priority' => 0.4, 'changeFreq' => 'monthly', 'lastmod' => ($blog = Blog::latest('updated_at')->first()) ? ($blog->updated_at->gt($blog->created_at) ? $blog->updated_at->toIso8601String() : $blog->created_at->toIso8601String()) : Carbon::now()->toIso8601String()),
             array('url' => url(asset($this->siteMapDirectory . '/category.xml')), 'priority' => 0.6, 'changeFreq' => 'monthly', 'lastmod' => ($category = Category::latest('updated_at')->first()) ? ($category->updated_at->gt($category->created_at) ? $category->updated_at->toIso8601String() : $category->created_at->toIso8601String()) : Carbon::now()->toIso8601String()),
-            array('url' => url(asset($this->siteMapDirectory . '/stores-images.xml')), 'priority' => 0.4, 'changeFreq' => 'monthly', 'lastmod' => ($store = Store::latest('updated_at')->first()) ? ($store->updated_at->gt($store->created_at) ? $store->updated_at->toIso8601String() : $store->created_at->toIso8601String()) : Carbon::now()->toIso8601String()),
+            // array('url' => url(asset($this->siteMapDirectory . '/stores-images.xml')), 'priority' => 0.4, 'changeFreq' => 'monthly', 'lastmod' => ($store = Store::latest('updated_at')->first()) ? ($store->updated_at->gt($store->created_at) ? $store->updated_at->toIso8601String() : $store->created_at->toIso8601String()) : Carbon::now()->toIso8601String()),
             array('url' => url(asset($this->siteMapDirectory . '/stores.xml')), 'priority' => 1.0, 'changeFreq' => 'weekly', 'lastmod' => ($store = Store::latest('updated_at')->first()) ? ($store->updated_at->gt($store->created_at) ? $store->updated_at->toIso8601String() : $store->created_at->toIso8601String()) : Carbon::now()->toIso8601String()),
-            array('url' => url(asset($this->siteMapDirectory . '/category-images.xml')), 'priority' => 0.4, 'changeFreq' => 'monthly', 'lastmod' => ($category = Category::latest('updated_at')->first()) ? ($category->updated_at->gt($category->created_at) ? $category->updated_at->toIso8601String() : $category->created_at->toIso8601String()) : Carbon::now()->toIso8601String()),
+            // array('url' => url(asset($this->siteMapDirectory . '/category-images.xml')), 'priority' => 0.4, 'changeFreq' => 'monthly', 'lastmod' => ($category = Category::latest('updated_at')->first()) ? ($category->updated_at->gt($category->created_at) ? $category->updated_at->toIso8601String() : $category->created_at->toIso8601String()) : Carbon::now()->toIso8601String()),
         ];
         generate_sitemap($data, $this->siteMapDirectory, 'sitemap.xml');
     }
